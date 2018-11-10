f17d43453a752362d6de2e5a83026bf6b9a37a93
p1pchenk0 <art.pipchenko@gmail.com>
2018-11-10 23:34:59 +0200

    * common.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-import Vuex from 'vuex'
+'use strict';
 
-const originalCommitFn = Vuex.Store.prototype.commit
+var _vuex = require('vuex');
 
-Vuex.Store.prototype.commit = function commit(_type, _payload, _options) {
+var _vuex2 = _interopRequireDefault(_vuex);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var originalCommitFn = _vuex2.default.Store.prototype.commit;
+
+_vuex2.default.Store.prototype.commit = function commit(_type, _payload, _options) {
     if (Array.isArray(_type)) {
         for (var i = 0, len = _type.length; i < len; i++) {
-            typeof _type[i] === 'string'
-                ? originalCommitFn.call(this, _type[i], undefined, _options)
-                : commit.call(this, _type[i])
+            typeof _type[i] === 'string' ? originalCommitFn.call(this, _type[i], undefined, _options) : commit.call(this, _type[i]);
         }
     } else if (_type.toString() === '[object Object]') {
         for (var type in _type) {
@@ -16,4 +20,4 @@ Vuex.Store.prototype.commit = function commit(_type, _payload, _options) {
     } else {
         originalCommitFn.call(this, _type, _payload, _options);
     }
-}
+};
